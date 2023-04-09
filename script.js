215e46bf9a36f2a4c13bb9f68ab0587e9dc0ef48
Mateo Ibata Losada <mateoibatalosada@MacBook-Air-de-Mateo-3.local>
2023-04-08 23:45:17 -0500

    me falta el boton y que funcione el evento

--- script.js
+++
@@ -1,9 +0,0 @@
-//Función para redirigir a otra página
-function initiate() {
-    window.location.href = 'index.html';
-}
-
-//Botón de iniciar
-const boton = document.getElementById('iniciar')
-
-boton.addEventListener('click', initiate);
