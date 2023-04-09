f0f1da9f0ff4622dbe250468c283ba56ae0698d6
Mateo Ibata Losada <mateoibatalosada@MacBook-Air-de-Mateo-3.local>
2023-04-09 00:36:45 -0500

    script listo

--- muro.js
+++ muro.js
@@ -1,8 +1,6 @@
 //Constantes
 const perfil = document.querySelector("#perfil")
-const input = document.querySelector("#input")
 const button = document.querySelector("#button")
-const boton = document.querySelector("#boton")
 
 /*Donde se escribe el muro*/
 const post = document.getElementById('post')
@@ -32,39 +30,40 @@ button.addEventListener('click', function (event) {
     </section>
     <button id="boton">Publicar</button>
     `
+
   modal.appendChild(contenido);
   document.body.appendChild(modal);
 
-  const cerrar = document.querySelector('.close');
-  cerrar.addEventListener('click', function () {
-    modal.style.display = 'none';
+  const boton = document.querySelector("#boton")
+  const input = document.querySelector("#input")
+  boton.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log(event.target.id);
+  
+    //pub es el "texto escrito"=value dentro del input que se hizo en el HTML
+    const pub = input.value;
+  
+    //Crear elemento div y p cada vez que se presione el botón
+    const bloque = document.createElement('div');
+    bloque.classList.add("chat");
+  
+    const texto = document.createElement("p");
+    texto.innerHTML = pub;
+    //El texto=p es hijo del bloque=div
+    bloque.appendChild(texto);
+  
+    //Bloque se encuentra adentro de post
+    post.appendChild(bloque);
 
+    input.value = "";
   });
-})
+
+  const cerrar = document.querySelector('.close');
+cerrar.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+});
 
 button.onclick = function () {
   modal.style.display = "block";
 }
-
-boton.addEventListener('click', function (event) {
-  event.preventDefault();
-  console.log(event.target.id);
-
-  //pub es el "texto escrito"=value dentro del input que se hizo en el HTML
-  const pub = input.value;
-
-  //Crear elemento div y p cada vez que se presione el botón
-  const bloque = document.createElement('div');
-  bloque.classList.add("chat");
-
-  const texto = document.createElement("p");
-  texto.innerHTML = pub;
-  //El texto=p es hijo del bloque=div
-  bloque.appendChild(texto);
-
-  //Bloque se encuentra adentro de post
-  post.appendChild(bloque);
-
-  //Una vez le doy click al botón, este es el valor al que regresa mi input
-  input.value = "";
-});
